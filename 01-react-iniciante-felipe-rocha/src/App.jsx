046d42c4753bdc4404df279780aee229b8050bde
Mateus Santos <mateussilvanzg@gmail.com>
2025-11-06 21:22:27 -0300

    feat(01-react-iniciante): implement useState and onTaskClick function

--- 01-react-iniciante-felipe-rocha/src/App.jsx
+++ 01-react-iniciante-felipe-rocha/src/App.jsx
@@ -1,12 +1,49 @@
+import { useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
 
 function App() {
+	const [tasks, setTasks] = useState([
+		{
+			id: 1,
+			title: "Estudar Inglês",
+			description: "Estudar inglês por duas horas.",
+			isCompleted: false,
+		},
+		{
+			id: 2,
+			title: "Estudar Programação",
+			description: "Estudar programação por duas horas.",
+			isCompleted: false,
+		},
+		{
+			id: 3,
+			title: "Estudar Álgebra",
+			description: "Estudar Álgebra por duas horas.",
+			isCompleted: false,
+		},
+	]);
+
+	function onTaskClick(taskId) {
+		const newTasks = tasks.map((task) => {
+			if (taskId == task.id) {
+				return { ...task, isCompleted: !task.isCompleted };
+			}
+
+			return task;
+		});
+
+		setTasks(newTasks);
+	}
+
 	return (
-		<div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-			<h1 className="text-blue-500 text-3xl">Gerenciador de Tarefas</h1>
-			<AddTask />
-			<Tasks />
+		<div className="w-screen h-screen bg-slate-500 flex justify-center flex-col items-center gap-1 p- 6">
+			<h1 className="text-slate-100 font-bold text-center text-3xl">Gerenciador de Tarefas</h1>
+
+			<AddTask className="w-[500]" />
+
+			<Tasks tasks={tasks} onTaskClick={onTaskClick} />
+			<div className="w-[500px]"></div>
 		</div>
 	);
 }
--- 01-react-iniciante-felipe-rocha/src/components/Tasks.jsx
+++ 01-react-iniciante-felipe-rocha/src/components/Tasks.jsx
@@ -1,5 +1,30 @@
-function Tasks() {
-	return <h1>Tasks</h1>;
+import PropTypes from "prop-types";
+
+function Tasks({ tasks, onTaskClick }) {
+	console.log(tasks);
+	return (
+		<ul className="space-y-2 w-[500px] bg-slate-200 rounded-md shadow-md p-4">
+			{tasks.map((task) => (
+				<li key={task.id} className="flex gap-2">
+					<button
+						onClick={() => onTaskClick(task.id)}
+						className={`bg-slate-400 w-full text-left rounded-md text-white p-2  ${task.isCompleted && "line-through"}`}
+						type="button"
+					>
+						{task.title}
+					</button>
+					<button className="bg-slate-400 p-2 rounded-md text-white" type="button">
+						▶️
+					</button>
+				</li>
+			))}
+		</ul>
+	);
 }
+
+Tasks.propTypes = {
+	tasks: PropTypes.array.isRequired,
+	onTaskClick: PropTypes.func,
+};
 
 export default Tasks;

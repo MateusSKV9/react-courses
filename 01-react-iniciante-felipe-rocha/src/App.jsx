c03b1e9325d87e0322859c889c9298173644f86b
Mateus Santos <mateussilvanzg@gmail.com>
2025-11-08 14:03:48 -0300

    feat(01-react-iniciante): add data persistence with localStorage

--- 01-react-iniciante-felipe-rocha/src/App.jsx
+++ 01-react-iniciante-felipe-rocha/src/App.jsx
@@ -1,29 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
 import { v4 as uuidv4 } from "uuid";
+import Title from "./components/Title";
 
 function App() {
-	const [tasks, setTasks] = useState([
-		{
-			id: 1,
-			title: "Estudar Inglês",
-			description: "Estudar inglês por duas horas.",
-			isCompleted: false,
-		},
-		{
-			id: 2,
-			title: "Estudar Programação",
-			description: "Estudar programação por duas horas.",
-			isCompleted: false,
-		},
-		{
-			id: 3,
-			title: "Estudar Álgebra",
-			description: "Estudar Álgebra por duas horas.",
-			isCompleted: false,
-		},
-	]);
+	const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")) || []);
 
 	function onTaskClick(taskId) {
 		const newTasks = tasks.map((task) => {
@@ -36,6 +18,26 @@ function App() {
 
 		setTasks(newTasks);
 	}
+
+	useEffect(() => {
+		localStorage.setItem("tasks", JSON.stringify(tasks));
+	}, [tasks]);
+
+	// useEffect(() => {
+	// 	const fetchTasks = async () => {
+	// 		//CHAMA A API
+	// 		const response = await fetch("https://jsonplaceholder.typicode.com/todos?_limit=10", { method: "GET" });
+
+	// 		// PEGA DOS DADOS RETORNADOS
+	// 		const data = await response.json();
+	// 		console.log(data);
+
+	// 		// ARMAZENA/PERSISTE OS DADOS NO STATE
+	// 		setTasks(data);
+	// 	};
+
+	// 	fetchTasks();
+	// }, []);
 
 	function onDeleteTaskClick(taskId) {
 		const newTasks = tasks.filter((task) => taskId != task.id);
@@ -53,8 +55,8 @@ function App() {
 	}
 
 	return (
-		<div className="w-screen h-screen bg-slate-500 flex justify-center flex-col items-center gap-4 p-6">
-			<h1 className="text-slate-100 font-bold text-center text-3xl">Gerenciador de Tarefas</h1>
+		<div className="w-screen min-h-screen bg-slate-500 flex justify-center flex-col items-center gap-4 p-6">
+			<Title>Gerenciador de Tarefas</Title>
 
 			<AddTask onAddTaskSubmit={onAddTaskSubmit} className="w-[500]" />
 

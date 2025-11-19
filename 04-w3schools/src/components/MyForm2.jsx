718b940988c22054fea214a63df33c25b9f0e89c
Mateus Santos <mateussilvanzg@gmail.com>
2025-11-19 19:03:46 -0300

    feat(04-w3schools): add checkbox

--- 04-w3schools/src/components/MyForm2.jsx
+++ 04-w3schools/src/components/MyForm2.jsx
@@ -4,17 +4,30 @@ function MyForm2() {
 	const [inputs, setInputs] = useState({
 		firstname: "John",
 		lastname: "Doe",
+		tomato: true,
+		onion: false,
 	});
 
 	const handleChange = (e) => {
-		const name = e.target.name;
-		const value = e.target.value;
+		const target = e.target;
+		const name = target.name;
+		const value = target.type === "checkbox" ? target.checked : target.value;
 		setInputs((values) => ({ ...values, [name]: value }));
 	};
 
 	function handleSubmit(e) {
 		e.preventDefault();
 		alert(`Form: ${JSON.stringify(inputs)}`);
+
+		let fillings = "";
+		if (inputs.tomato) fillings += "Tomato";
+		if (inputs.onion) {
+			if (inputs.tomato) fillings += " and ";
+			fillings += "onion";
+		}
+
+		if (fillings == "") fillings = "no fillings";
+		alert(`${inputs.firstname} wants a burguer with ${fillings}`);
 	}
 
 	return (
@@ -28,6 +41,17 @@ function MyForm2() {
 				Last Name:
 				<input onChange={handleChange} value={inputs.lastname} type="text" name="lastname" id="lastname" />
 			</label>
+
+			<p>
+				<label htmlFor="tomato">
+					Tomato:
+					<input onChange={handleChange} checked={inputs.tomato} type="checkbox" name="tomato" id="tomato" />
+				</label>
+				<label htmlFor="onion">
+					Onion:
+					<input onChange={handleChange} checked={inputs.onion} type="checkbox" name="onion" id="onion" />
+				</label>
+			</p>
 
 			<p>
 				Current values: {inputs.firstname} {inputs.lastname}
